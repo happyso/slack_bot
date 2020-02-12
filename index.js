c2ee1a406774dd910cbc07d35f1c4ec806aea01f
김소영 <cheza1220@gmail.com>
2020-02-12 17:31:56 +0900

    setting webhook

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const Slack = require('slack-node');  // 슬랙 모듈 사용
+const webhookUri = "https://hooks.slack.com/services/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";  // Webhook URL
 
-apiToken = "xoxp-948166928677-938005983073-950370745238-eb32ac25b1f0cf898617580548c3e177";
-const slack = new Slack(apiToken);
+const slack = new Slack();
+slack.setWebhook(webhookUri);
 
 const send = async(message) => {
-    slack.api('chat.postMessage', {
-        username: 'dev-test',  // 슬랙에 표시될 봇이름
-        text:message,
-        channel:'#general'  // 전송될 채널 및 유저
+    slack.webhook({
+        text:"인터넷 검색 포털 사이트",
+        attachments:[
+            {
+                fallback:"링크주소: <https://www.google.com|구글>",
+                pretext:"링크주소: <https://www.google.com|구글>",
+                color:"#00FFFF",
+                fields:[
+                    {
+                        title:"알림",
+                        value:"해당링크를 클릭하여 검색해 보세요.",
+                        short:false
+                    }
+                ]
+            }
+        ]
     }, function(err, response){
         console.log(response);
     });
-};
-
-send('메세지 내용');
+}
